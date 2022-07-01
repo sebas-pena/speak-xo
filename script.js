d8bb7ca38dc17c758ed94837f60ce3292aebd781
Pablo Peña <gmlpubli00@gmail.com>
2022-06-30 22:04:21 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const mouthCanvas = document.querySelector("#mouth")
+const eyes = document.querySelectorAll(".eye > div")
+
+const position = eyes[0].getBoundingClientRect()
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX
+  const y = e.clientY
+  console.clear()
+  console.log(position)
+  console.log("x", x, "y", y)
+})
